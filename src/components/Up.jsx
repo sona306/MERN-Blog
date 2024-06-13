4efd7352ca26826971156ed98149c4de036b0e6c
sona306 <sonasabu307@gmail.com>
2024-06-13 14:27:26 +0530

    signup ui

---
+++ src/components/Up.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+
+const Up = () => {
+
+    const [signup,setData]=useState(
+        {
+            "name":"",
+            "emailid":"",
+            "password":""
+        }
+    )
+    const inputHandler=(event)=>{
+        setData({...signup,[event.target.name]:event.target.value})
+    }
+    const readValue=()=>{
+        console.log(signup)
+    }
+  return (
+    <div>
+        <container>
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <row>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Name</label>
+                        <input type="text" className="form-control" name='name' value={signup.name} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Email</label>
+                        <input type="text" className="form-control" name='emailid' value={signup.emailid} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <label htmlFor="" className="form-label">Password</label>
+                        <input type="text" className="form-control" name='password' value={signup.password} onChange={inputHandler}/>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><br></br>
+                            <button className="btn btn-primary" onClick={readValue} >Register</button>
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><br></br>
+                        <button className="btn btn-warning" >Back signIN</button>
+                        </div>
+                    </row>
+                </div>
+            </div>
+        </container>
+    </div>
+  )
+}
+
+export default Up
